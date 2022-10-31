237bea2a3298336fb325df19a7edf588a386b29f
Elias Torres <eliasomar.torres@gmail.com>
2022-10-30 23:00:31 -0700

    Agregada la adquisicion de informacion

--- Actividad3.7/casa-de-cambio.py
+++ Actividad3.7/casa-de-cambio.py
@@ -1,3 +1,5 @@
+# Este programa cambia el cheque de los trabajadores 
+# de pesos a dolares y cobra una comision de 70 MXN
 tipoDeCambio = 19.5
 COMISION = 70
 NOMBRE_CASA = 'Casa de cambio Playas'
@@ -5,3 +7,20 @@ nombreCliente = ''
 lugarTrabajo = ''
 dolares = 0
 pesos = 0
+
+# Aqui se muestra el mensaje de bienvenida
+# y el nombre de la casa de cambio
+
+print("Bienvenido a ",NOMBRE_CASA)
+print("Este programa convierte su cheque en dolares a pesos")
+
+# Solicitamos el nombre y lugar de trabajo al 
+
+nombreCliente = input("Favor de introducir su nombre: ")
+lugarTrabajo = input("Favor de introducir su lugar de trabajo")
+
+# Aqui solicitamos la cantidad de dolares a cambiar
+
+dolares = int(input("Introduzca los dolares de su cheque"))
+
+# Convertimos la cantidad a pesos y restamos la comision
