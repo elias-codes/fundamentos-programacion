252a29ad031a9355e73892b3e08078472829a094
Elias Torres <eliasomar.torres@gmail.com>
2022-10-30 22:05:28 -0700

    Archivo creado

--- holamundo.py
+++ holamundo.py
@@ -1,2 +1,4 @@
-msg = "Hola mundo"
-print(msg)
+horas = int(input('Introduce el numero de horas: '))
+costoPorHora = int(input("Introduce el costo por hora: "))
+pago =  horas * costoPorHora
+print ("El pago sera de",pago, "dolares")
