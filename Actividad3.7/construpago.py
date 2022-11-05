be21e8c805ac2fed7ed555bb18497096760fa6f4
Elias Torres <eliasomar.torres@gmail.com>
2022-11-05 10:05:45 -0700

    Agrega constantes, mensaje de bienvenida y captura de datos

---
+++ Actividad3.7/construpago.py
@@ -0,0 +1,15 @@
+# Este programa indica la cantidad a pagar a un 
+# trabajador de contruccion por dia
+VALOR_BLOQUE = 4
+VALOR_DESPERDICIO = 6
+
+#Mensaje de bienvenida
+
+print("Bienvenido a ConstruPay")
+print("Este programa calcula la cantidad a pagar al trabajador")
+
+#Se le piden los datos al operador
+
+bloques = int(input("Favor de introducir la cantidad de bloques colocados: "))
+desperdicio = int(input("Introduzca la cantidad de bloques desperdiciados: "))
+
