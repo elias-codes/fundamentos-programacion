b9a422546e5cf23a42cc365dbdbb6943df97c670
Elias Torres <eliasomar.torres@gmail.com>
2022-12-03 18:04:11 -0800

    Agrega ciclo while

--- Examen/examen.py
+++ Examen/examen.py
@@ -7,15 +7,17 @@ TECATE = 70000
 SANQ = 50000
 #Imprimimos el mensaje de bienvenida
 print("Bienvenido a SUPERSAT")
-#pedimos los datos
-nombre = input("Introduzca su nombre: ")
-edad = int(input("Edad: "))
-sexo = input("Genero: ")
-ingreso = int(input("Ingreso mensual: "))
-municipio = input("""Seleccione su municipio:
-Tijuana = T
-Ensenada = E
-Mexicali = M
-Tecate = T
-San Quintin = S 
-""")
+#Abrimos un while para repetir el programa
+while (True):
+    #pedimos los datos
+    nombre = input("Introduzca su nombre: ")
+    edad = int(input("Edad: "))
+    sexo = input("Genero: ")
+    ingreso = int(input("Ingreso mensual: "))
+    municipio = input("""Seleccione su municipio:
+    Tijuana = T
+    Ensenada = E
+    Mexicali = M
+    Tecate = T
+    San Quintin = S 
+    """)
